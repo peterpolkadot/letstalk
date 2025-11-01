5f6d3fd6838a706d8eaed91b05e57ce111e792f0
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-11-02 08:45:40 +1000

    🧱 Init or update: components/Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Footer from './Footer';
+import '../styles/globals.css';
+export default function Layout({ children }) {
+  return (<div className="min-h-screen flex flex-col"><Header/><main className="flex-1 container mx-auto p-6">{children}</main><Footer/></div>);
+}
