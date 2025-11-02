eddee890dfb6663bba8b78ea235b5d3a7f9091cd
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-11-02 14:54:36 +1000

    🧱 Init or update: components/Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className='bg-gray-900 text-white py-4'>
       <div className='container mx-auto flex justify-center'>
-        <a href='/' className='text-xl font-bold tracking-wide'>Chatbot City</a>
+        <a href='/' className='text-xl font-bold tracking-wide'>🤖 Chatbot City</a>
       </div>
     </header>
   );
